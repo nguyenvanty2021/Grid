9a21fee102d78557377340b995e0157cd28fd2de
Tỷ Nguyễn <tynguyen@Tys-MacBook-Pro.local>
2022-05-03 19:24:41 +0700

    upadte

--- src/App.js
+++ src/App.js
@@ -1,23 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import img1 from "./image/img1.png";
+import img2 from "./image/img2.png";
+import img3 from "./image/img3.png";
+import img4 from "./image/img4.png";
+import img5 from "./image/img5.png";
+import img6 from "./image/img6.png";
+import img7 from "./image/img7.png";
+import img8 from "./image/img8.png";
+import img9 from "./image/img9.png";
+import img10 from "./image/img10.png";
+import img11 from "./image/img11.png";
+import img12 from "./image/img12.png";
+import img13 from "./image/img13.png";
+import img14 from "./image/img14.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span>
+        <img src={img1} />
+      </span>
+      <span>
+        <img src={img2} />
+      </span>
+      <ul class="list">
+        <li>EASIER TO CREATE TWO DIMENSIONAL LAYOUTS</li>
+        <li>SIMPLER MARKUP</li>
+        <li>FLEXIBLE</li>
+        <li>SKIP FRAMEWORKS</li>
+        <li>BROWSER SUPPORT</li>
+        <li>
+          <a href="examples.html">SEE EXAMPLES</a>
+        </li>
+      </ul>
+      <span>
+        <img src={img3} />
+      </span>
+      <span>
+        <img src={img4} />
+      </span>
+      <div class="container">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+      </div>
+      <span>
+        <img src={img5} />
+      </span>
+      <span>
+        <img src={img6} />
+      </span>
+      <div class="container1">
+        <div class="header">HEADER</div>
+        <div class="menu">MENU</div>
+        <div class="content">CONTENT</div>
+        <div class="footer">FOOTER</div>
+      </div>
+      <span>
+        <img src={img7} />
+      </span>
+      <span>
+        <img src={img8} />
+      </span>
+      <div class="container2">
+        <div class="header2">HEADER</div>
+        <div class="menu2">MENU</div>
+        <div class="content2">CONTENT</div>
+        <div class="footer2">FOOTER</div>
+      </div>
+      <span>
+        <img src={img9} />
+      </span>
+      <span>
+        <img src={img10} />
+      </span>
+      <div class="container3">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+        <div>10</div>
+        <div>11</div>
+        <div>12</div>
+      </div>
+      <span>
+        <img src={img11} />
+      </span>
+      <span>
+        <img src={img12} />
+      </span>
+      <div class="container4">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>8</div>
+        <div>9</div>
+        <div>10</div>
+        <div>11</div>
+        <div>12</div>
+      </div>
+      <span>
+        <img src={img13} />
+      </span>
+      <span>
+        <img src={img14} />
+      </span>
+      <div class="grid-page5">
+        <div class="header5">HEADER</div>
+        <div class="menu5">MENU</div>
+        <div class="content5">CONTENT</div>
+        <div class="footer5">FOOTER</div>
+      </div>
     </div>
   );
 }
